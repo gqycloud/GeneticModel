65adeea3bbc316799824365c8957f5aabdd38ac2
Gina Wang <14085239+gqycloud@users.noreply.github.com>
2019-09-21 16:31:03 -0400

    Update GeneticAlgorithmn.py

--- GeneticAlgorithmn.py
+++ GeneticAlgorithmn.py
@@ -3,44 +3,39 @@ import random
 import matplotlib.pyplot as plot
 
 def main():  
+
+  starting_population = [
+     {'group': 'male_prey', 'number': 10, 'color': get_random_color, 'max_age': 2, 'interaction': male_interaction},
+     {'group': 'female_prey', 'number': 10, 'color': get_random_color, 'max_age': 2, 'interaction': reproduce},
+     {'group': 'predators', 'number': 10, 'color': get_predator_color, 'max_age': 5, 'interaction': eat_prey}
+     ]
     
-  #Starting population of prey and predator organisms 
-  starting_males = 10
-  starting_females = 10
-  predators = 10
-  
   print('generating population...')
-  population= initialize_population(starting_males, starting_females, predators)
+  population= initialize_population(starting_population)
   print('beginning population...')
   plot_population(population)
   print('begin interactions ...')
-  population = populations_interact(population, 20)
+  population = populations_interact(population, 10)
   print('final population')
   plot_population(population)
-  
-  
-def initialize_population(starting_males, starting_females, predators): 
+
+#Initialization 
+def initialize_population(starting_population): 
     population = []
-    for i in range(starting_males):
-        x = random.randint(-50, 50)
-        y = random.randint(-50, 50) 
-        color = random.randint(0, 50000) 
-        group = 'male_prey'
-        population.append(Organism(group, x, y, color))
-    for i in range(starting_females):
-        x = random.randint(-50, 50)
-        y = random.randint(-50, 50) 
-        color = random.randint(0, 50000) 
-        group = 'female_prey'
-        population.append(Organism(group, x, y, color))
-    for i in range(predators):
-        x = random.randint(-50, 50)
-        y = random.randint(-50, 50) 
-        color = random.randint(0, 50000) 
-        group = 'predators'
-        population.append(Organism(group, x, y, color))   
+    for organism_type in starting_population: 
+        for organism in range(0, organism_type['number']): 
+             x = random.randint(-100, 100)
+             y = random.randint(-100, 100)
+             population.append(Organism(
+                     x, 
+                     y, 
+                     organism_type['group'], 
+                     organism_type['max_age'], 
+                     organism_type['color'](), 
+                     organism_type['interaction']))
     return population
-      
+  
+#Interacting    
 def populations_interact(population, cycles): 
     for i in range(0,cycles): 
        population = population_move(population)
@@ -56,7 +51,7 @@ def population_move(population):
 
 def population_interact(population): 
     for organism in population: 
-        organism.interact(population)
+        organism.interact(organism, population)
     return population
 
 def clean_population(population): 
@@ -64,107 +59,125 @@ def clean_population(population):
         organism.die(population)
     return population
 
-
+#Visualization
 def plot_population(population): 
-    x = []
-    y = []
-    color = []
+    predator_x = []
+    predator_y = []
+    predator_color = []
+
+    prey_x = []
+    prey_y = []
+    prey_color = []
     for organism in population: 
-        x.append(organism.x)
-        y.append(organism.y)
-        color.append(organism.color)
-        
+        if(organism.group == 'predators'): 
+            predator_x.append(organism.x)
+            predator_y.append(organism.y)
+            predator_color.append(organism.color)
+        else: 
+            prey_x.append(organism.x)
+            prey_y.append(organism.y)
+            prey_color.append(organism.color)
+    
     fig, ax = plot.subplots()
-    ax.scatter(x , y, c=color)
+    ax.scatter(prey_x, prey_y, c=prey_color, marker='o')
+    ax.scatter(predator_x, predator_y, c=predator_color, marker='*')
     
     ax.set_title('Population Plot')
     ax.grid(True)
     fig.tight_layout()
     plot.show()
 
-class Organism(): 
-    #Each organism will have a type and a starting location in the 'environment'
-    #The color and the age only matter for the prey population    
-    def __init__(self, group, startx, starty, color):
+#Organism Modeling 
+class Organism():   
+    def __init__(self, startx, starty, group, max_age, color, interaction):
         self.group = group
         self.x = startx
         self.y = starty
         self.age = 0
-        if(group == 'predator'): 
-            self.color = 99999
-        else:
-            self.color = color
+        self.color = color
+        self.interact = interaction
+        self.max_age = max_age
         
     #At each iteration all the organisms will move randomly
     def move(self): 
-        self.x += random.randint(-5, 5) #start with 5 for now we can make this a variable later
+        self.x += random.randint(-5, 5) 
         self.y += random.randint(-5, 5)
         self.age += 1
         
     #At the end of each iteration we will check if the organism has died of old age
     def die(self, population):
-        if self.age > 10: #we can play around with this 
+        if self.age > self.max_age:
             population.remove(self)
-            
-    def interact(self, population): 
-        #Generate a list of prey neighbors
-        neighbors = filter(lambda population: filter_neighbors(self, population), population)
-        preys = filter(filter_prey, neighbors)        
-        
-        if self.group == 'female_prey':            
-                if (self.age > 3): #reproductive age 
-                   population = reproduce(self, preys, population)
-        elif self.group == 'predator':
-            population = eat_prey(population, preys)
 
+# Filtering functions
+def get_nearby_preys(target, population): 
+        neighbors = list(filter(lambda population: filter_neighbors(target, population), population))
+        preys = list(filter(filter_prey, neighbors) ) 
+        return preys
 
-def eat_prey(population, preys): 
-     most_colorful = max(preys, key=get_most_colorful)
-     population.remove(most_colorful)
-     return population
-            
-def reproduce(target_prey, neighbor_preys, population):
-    reproductive_preys = filter(filter_reproductive_prey, neighbor_preys)
-    reproductive_male_preys = filter(filter_male_prey, reproductive_preys)
-    most_colorful_reproductive = max(reproductive_male_preys, key=get_most_colorful)
-    for i in range(0, 1): #number of children per iteration
-        x = random.randint(-100, 100)
-        y = random.randint(-100, 100) 
-        color = (target_prey.color + most_colorful_reproductive.color)/2
-        random_group = random.randint(0, 2)
-        if (random_group == 0): 
-            group = 'female_prey'
-        else: 
-            group = 'male_prey'
-        population.append(Organism(group, x, y, color))
-    return population
-
-def filter_male_prey(organism):
-    if(organism.group == 'male_prey'): 
-        return organism 
-    
-def filter_reproductive_prey(organism): 
-    if(organism.age > 3): 
-        return organism
-      
-def filter_prey(organism): 
-    if(organism.group != 'predator'): 
-        return organism
-                
 def filter_neighbors(organism, neighbor):
     distance = math.sqrt(abs(organism.x-neighbor.x)^2 + abs(organism.y-neighbor.y)^2);
     if distance < 10: #start with 10 for now, we can make this a variable later
-        return neighbor
-
+        return True
+    else: 
+        return False
+    
+def filter_prey(organism): 
+    if(organism.group != 'predators'): 
+        return True
+    else: 
+        return False
+    
+def filter_male_prey(organism):
+    if(organism.group == 'male_prey'): 
+        return True
+    else: 
+        return False
+      
 def get_most_colorful(prey): 
-    return prey.color
+    return prey.color      
         
-        
-        
-        
-        
-        
-        
+#Prey functions             
+def get_random_color(): 
+    return random.randint(0, 50000)   
+
+def reproduce(target_prey, population):
+    reproduction_options = get_nearby_male_prey(target_prey, population)
+    if(reproduction_options): 
+        reproduction_canidate = max(reproduction_options, key=get_most_colorful)
+        for i in range(0, 1): #number of children per iteration
+            x = random.randint(-100, 100)
+            y = random.randint(-100, 100) 
+            color = (target_prey.color + reproduction_canidate.color)/2
+            random_group = random.randint(0, 2)
+            if (random_group == 0): 
+                group = 'female_prey'
+                interaction = reproduce
+            else: 
+                group = 'male_prey'
+                interaction = male_interaction
+            population.append(Organism(x, y, group, 10, color, interaction))
+    return population
+
+def get_nearby_male_prey(target, population): 
+    prey_neighbors = get_nearby_preys(target, population)  
+    male_prey = list(filter(filter_male_prey, prey_neighbors))
+    return male_prey
+
+def male_interaction(organism, population): 
+    return population
+
+#Predator functions
+def get_predator_color(): 
+    return 99999             
+     
+def eat_prey(target_predator, population):    
+     prey_neighbors = get_nearby_preys(target_predator, population)     
+     if(prey_neighbors): 
+         target_prey = max(prey_neighbors, key=get_most_colorful)
+         population.remove(target_prey)
+     return population
+               
         
         
         
